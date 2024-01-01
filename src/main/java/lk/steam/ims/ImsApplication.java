381040e48b0334a88f0892cba66be1fc84e39a51
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-01 13:28:50 +0530

    /Dashboard mapped

--- src/main/java/lk/steam/ims/ImsApplication.java
+++ src/main/java/lk/steam/ims/ImsApplication.java
@@ -2,6 +2,8 @@ package lk.steam.ims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class ImsApplication {
@@ -9,6 +11,12 @@ public class ImsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImsApplication.class, args);
 		System.out.println("IMS System is now running!");
+	}
+	@RequestMapping(value = "/Dashboard")
+	public ModelAndView imsDashboard(){
+		ModelAndView rmsView = new ModelAndView();
+		rmsView.setViewName("Dashboard.html");
+		return rmsView;
 	}
 
 }
