24cff967cf041d906efb1af9f834fbd1d1d4622b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-01 13:14:52 +0530

    manual project running custom log added

--- src/main/java/lk/steam/ims/ImsApplication.java
+++ src/main/java/lk/steam/ims/ImsApplication.java
@@ -8,6 +8,7 @@ public class ImsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImsApplication.class, args);
+		System.out.println("IMS System is now running!");
 	}
 
 }
