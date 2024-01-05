c3e0b8d536bb088fa2f608c061883787914c0478
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-05 19:07:34 +0530

    mapping user

--- src/main/java/lk/steam/ims/entity/User.java
+++ src/main/java/lk/steam/ims/entity/User.java
@@ -1,4 +1,29 @@
 package lk.steam.ims.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+    private String password;
+    private String email;
+    private LocalDateTime addedTime;
+    private String note;
+    private Employee employeeID;
+
 }
