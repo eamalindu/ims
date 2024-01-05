267ec3cd2cb3b47afb154bbff53ed3794af34620
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-05 19:38:47 +0530

    getEmployeesWithoutUserAccount() service implemented

--- src/main/java/lk/steam/ims/dao/EmployeeDAO.java
+++ src/main/java/lk/steam/ims/dao/EmployeeDAO.java
@@ -2,6 +2,11 @@ package lk.steam.ims.dao;
 
 import lk.steam.ims.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeDAO extends JpaRepository<Employee,Integer> {
+    @Query(value = "select e from Employee e where e.id not in (select u.employeeID from User u where u.employeeID is not null)")
+    List<Employee> getEmployeesWithoutUserAccount();
 }
