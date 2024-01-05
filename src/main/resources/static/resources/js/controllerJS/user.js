413bcc7f86760b61c311dd3d8245ab5c451543c6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-05 20:15:41 +0530

    user js edited

--- src/main/resources/static/resources/js/controllerJS/user.js
+++ src/main/resources/static/resources/js/controllerJS/user.js
@@ -1,5 +1,9 @@
 window.addEventListener('load',()=>{
+    employeesWithoutUserAccounts = ajaxGetRequest("/Employee/GetEmployeesWithoutUserAccount");
+    fillSelectOptions(userEmployee, 'Please Select an Employee', employeesWithoutUserAccounts, 'fullName')
 
     $('#userEmployee').chosen({width:'100%'});
     $('#userRole').chosen({width:'100%',placeholder_text_multiple: "Please Select At Least One Role",min_selected_options:1});
+
+
 });
