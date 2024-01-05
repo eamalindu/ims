74271b26069f65f6fff428af574f7c018953437b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-05 19:17:31 +0530

    get mapping added

--- src/main/java/lk/steam/ims/controller/UserController.java
+++ src/main/java/lk/steam/ims/controller/UserController.java
@@ -1,4 +1,23 @@
 package lk.steam.ims.controller;
 
+import lk.steam.ims.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value = "/User")
 public class UserController {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @GetMapping
+    public ModelAndView userUI(){
+        ModelAndView userView = new ModelAndView();
+        userView.setViewName("user.html");
+        return  userView;
+    }
 }
