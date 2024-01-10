6448479bc5e9ed2fee76b5115a33a5aa0a77ccf9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-10 19:29:25 +0530

    renamed

--- src/main/java/lk/steam/ims/dao/RoleDAO.java
+++ src/main/java/lk/steam/ims/dao/RoleDAO.java
@@ -4,6 +4,6 @@ package lk.steam.ims.dao;
 import lk.steam.ims.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface roleDAO extends JpaRepository<Role,Integer> {
+public interface RoleDAO extends JpaRepository<Role,Integer> {
 
 }
