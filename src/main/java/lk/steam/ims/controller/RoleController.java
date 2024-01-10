4fbfa52dc6238374a56e450e8d4cad1e14601da2
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-10 19:32:24 +0530

    complete the findall mapping

--- src/main/java/lk/steam/ims/controller/RoleController.java
+++ src/main/java/lk/steam/ims/controller/RoleController.java
@@ -1,9 +1,14 @@
 package lk.steam.ims.controller;
 
 import lk.steam.ims.dao.RoleDAO;
+import lk.steam.ims.entity.Batch;
+import lk.steam.ims.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/role")
@@ -12,5 +17,8 @@ public class RoleController {
     @Autowired
     private RoleDAO roleDAO;
 
-
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Role> findAll(){
+        return roleDAO.findAll();
+    }
 }
