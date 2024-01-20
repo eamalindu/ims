ee8a3d18338dfd15181a1bdfa1864f2234d71a01
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-20 20:38:04 +0530

    checkBoxValidator added

--- src/main/resources/static/resources/js/controllerJS/privilege.js
+++ src/main/resources/static/resources/js/controllerJS/privilege.js
@@ -4,3 +4,13 @@ window.addEventListener('load', () => {
     $('#privilegeRole').chosen({width: '100%'});
     $('#privilegeModule').chosen({width: '100%'});
 });
+
+const checkBoxValidator = (elementID,leftDivID,rightDivID) => {
+    if (elementID.checked) {
+        rightDivID.classList.add('bg-success', 'text-white');
+        leftDivID.classList.remove('bg-success', 'text-white')
+    } else {
+        rightDivID.classList.remove('bg-success', 'text-white');
+        leftDivID.classList.add('bg-success', 'text-white')
+    }
+}
