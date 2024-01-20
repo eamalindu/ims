50cd20f93aa4ab1ef35128d5cb6e9a241d2190a9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-20 21:13:05 +0530

    resetPrivilegeForm edited

--- src/main/resources/static/resources/js/controllerJS/privilege.js
+++ src/main/resources/static/resources/js/controllerJS/privilege.js
@@ -52,4 +52,7 @@ const resetPrivilegeForm = ()=>{
     //initialize the 3rd party libraries (chosen)
     $('#privilegeRole').chosen({width: '100%'});
     $('#privilegeModule').chosen({width: '100%'});
+
+    //reset privilege object
+    newPrivilege = {}
 }
