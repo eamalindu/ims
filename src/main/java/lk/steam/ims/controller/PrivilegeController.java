8fa620dc3b77393715564be67da02365e3a8075d
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-20 20:21:55 +0530

    UI service added

--- src/main/java/lk/steam/ims/controller/PrivilegeController.java
+++ src/main/java/lk/steam/ims/controller/PrivilegeController.java
@@ -1,9 +1,24 @@
 package lk.steam.ims.controller;
 
+import lk.steam.ims.dao.PrivilegeDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value = "/Privilege")
 public class PrivilegeController {
+
+    @Autowired
+    private PrivilegeDAO privilegeDAO;
+
+    @GetMapping
+    public ModelAndView privilegeUI(){
+        ModelAndView privilegeView = new ModelAndView();
+        privilegeView.setViewName("Privilege.html");
+        return  privilegeView;
+    }
+
 }
