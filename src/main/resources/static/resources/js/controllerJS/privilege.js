e8eb7558f20105104318d290f4f774dd4e2bc242
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-20 20:35:34 +0530

    chosen initialized

---
+++ src/main/resources/static/resources/js/controllerJS/privilege.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+
+    //initialize the 3rd party libraries (chosen)
+    $('#privilegeRole').chosen({width: '100%'});
+    $('#privilegeModule').chosen({width: '100%'});
+});
