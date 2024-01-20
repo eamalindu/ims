8c809feb4f6a3040be0c5d51a4b11ed8507ef14a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-20 20:19:59 +0530

    controller edited

--- src/main/java/lk/steam/ims/controller/PrivilegeController.java
+++ src/main/java/lk/steam/ims/controller/PrivilegeController.java
@@ -1,4 +1,9 @@
 package lk.steam.ims.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Privilege")
 public class PrivilegeController {
 }
