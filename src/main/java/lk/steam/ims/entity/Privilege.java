686c18279c65ad3e50821dd6e91588c780836ce2
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-20 20:03:10 +0530

    lombok added

--- src/main/java/lk/steam/ims/entity/Privilege.java
+++ src/main/java/lk/steam/ims/entity/Privilege.java
@@ -1,4 +1,18 @@
 package lk.steam.ims.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "privilege")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Privilege {
+
+
 }
