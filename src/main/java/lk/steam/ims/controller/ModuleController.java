ee138700647daee152eecc206faca5922c2c9ee5
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-21 11:26:35 +0530

    findall service added

--- src/main/java/lk/steam/ims/controller/ModuleController.java
+++ src/main/java/lk/steam/ims/controller/ModuleController.java
@@ -1,4 +1,23 @@
 package lk.steam.ims.controller;
 
+import lk.steam.ims.dao.ModuleDAO;
+import lk.steam.ims.entity.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "module")
 public class ModuleController {
+
+    @Autowired
+    private ModuleDAO moduleDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Module> findAll(){
+      return moduleDAO.findAll();
+    }
 }
--- src/main/java/lk/steam/ims/dao/ModuleDAO.java
+++ src/main/java/lk/steam/ims/dao/ModuleDAO.java
@@ -1,5 +1,6 @@
 package lk.steam.ims.dao;
 
+import lk.steam.ims.entity.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ModuleDAO extends JpaRepository<Module,Integer> {
