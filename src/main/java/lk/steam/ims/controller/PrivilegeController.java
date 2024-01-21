880867731ff411625f160f6bd0e706f9ee65d675
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-21 08:17:59 +0530

    findAll service mapped

--- src/main/java/lk/steam/ims/controller/PrivilegeController.java
+++ src/main/java/lk/steam/ims/controller/PrivilegeController.java
@@ -1,11 +1,14 @@
 package lk.steam.ims.controller;
 
 import lk.steam.ims.dao.PrivilegeDAO;
+import lk.steam.ims.entity.Privilege;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/Privilege")
@@ -19,6 +22,11 @@ public class PrivilegeController {
         ModelAndView privilegeView = new ModelAndView();
         privilegeView.setViewName("privileges.html");
         return  privilegeView;
+    }
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Privilege> findAll(){
+        return privilegeDAO.findAll();
     }
 
 }
