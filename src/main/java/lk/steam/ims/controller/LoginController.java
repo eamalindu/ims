dc845dc8a71ee15aedb6f7fae10e5d6e07f7ff11
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-22 19:14:35 +0530

    loginform service mapped

--- src/main/java/lk/steam/ims/controller/LoginController.java
+++ src/main/java/lk/steam/ims/controller/LoginController.java
@@ -1,7 +1,16 @@
 package lk.steam.ims.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class LoginController {
+
+    @GetMapping(value = "/loginfrom")
+    public ModelAndView loginUI(){
+        ModelAndView loginView = new ModelAndView();
+        loginView.setViewName("login.html");
+        return loginView;
+    }
 }
