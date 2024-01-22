1ac7ef48123f434df867ff6906e23efdaf86cd40
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-22 19:31:07 +0530

    getAllFollowUpsByInquiryId() new method edited

--- src/main/java/lk/steam/ims/dao/FollowUpDAO.java
+++ src/main/java/lk/steam/ims/dao/FollowUpDAO.java
@@ -14,7 +14,7 @@ public interface FollowUpDAO extends JpaRepository<FollowUp,Integer> {
     @Query(value = "SELECT f.* FROM followup f JOIN (SELECT inquiry_id, MAX(followuptime) AS max_followuptime FROM followup GROUP BY inquiry_id) max_followup ON f.inquiry_id = max_followup.inquiry_id AND f.followuptime = max_followup.max_followuptime ORDER BY inquiry_id;",nativeQuery = true)
     List<FollowUp> latestFollowupForEachInquiry();
 
-    @Query(value = "SELECT f from FollowUp f where f.inquiryId=?1")
+    @Query(value = "SELECT f from FollowUp f where f.inquiryId=?1 order by f.id desc")
     List<FollowUp> getAllFollowUpsByInquiryId(Inquiry inquiry);
 
 }
