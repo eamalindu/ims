04f0aee26d319f25fea39fcdd4ab6e0ff5d4e70c
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-01-22 19:26:47 +0530

    getAllFollowUpsByInquiryId() new method

--- src/main/java/lk/steam/ims/dao/FollowUpDAO.java
+++ src/main/java/lk/steam/ims/dao/FollowUpDAO.java
@@ -1,6 +1,7 @@
 package lk.steam.ims.dao;
 
 import lk.steam.ims.entity.FollowUp;
+import lk.steam.ims.entity.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,5 +13,8 @@ public interface FollowUpDAO extends JpaRepository<FollowUp,Integer> {
     //This data will be shown in crm-> Inquiries
     @Query(value = "SELECT f.* FROM followup f JOIN (SELECT inquiry_id, MAX(followuptime) AS max_followuptime FROM followup GROUP BY inquiry_id) max_followup ON f.inquiry_id = max_followup.inquiry_id AND f.followuptime = max_followup.max_followuptime ORDER BY inquiry_id;",nativeQuery = true)
     List<FollowUp> latestFollowupForEachInquiry();
+
+    @Query(value = "SELECT f from FollowUp f where f.inquiryId=?1")
+    List<FollowUp> getAllFollowUpsByInquiryId(Inquiry inquiry);
 
 }
