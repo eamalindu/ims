437198e30a522ecc473069db3087dd14373c8032
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 13:28:09 +0530

    web security turned on

--- src/main/java/lk/steam/ims/config/WebConfig.java
+++ src/main/java/lk/steam/ims/config/WebConfig.java
@@ -1,7 +1,9 @@
 package lk.steam.ims.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig {
 }
