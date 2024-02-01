be0ff67fa32e00987cee0b74cc4edf27dfd27d0d
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 15:01:18 +0530

    new services added

--- src/main/java/lk/steam/ims/dao/UserDAO.java
+++ src/main/java/lk/steam/ims/dao/UserDAO.java
@@ -2,6 +2,13 @@ package lk.steam.ims.dao;
 
 import lk.steam.ims.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserDAO extends JpaRepository<User,Integer> {
+
+    @Query(value = "SELECT u from User u where u.email=?1")
+    User getUserByEmail(String email);
+
+    @Query(value = "SELECT u from User u where u.username=?1")
+    User getUserByUsername(String username);
 }
