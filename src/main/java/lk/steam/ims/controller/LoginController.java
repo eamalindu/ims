1762a6d92e2624bf8fd0bebc7dbfdf088e37de46
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 13:52:36 +0530

    new services added

--- src/main/java/lk/steam/ims/controller/LoginController.java
+++ src/main/java/lk/steam/ims/controller/LoginController.java
@@ -1,6 +1,7 @@
 package lk.steam.ims.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,5 +13,19 @@ public class LoginController {
         ModelAndView loginView = new ModelAndView();
         loginView.setViewName("login.html");
         return loginView;
+    }
+
+    @GetMapping(value = "/error")
+    public ModelAndView errorUI(){
+        ModelAndView errorView = new ModelAndView();
+        errorView.setViewName("error.html");
+        return errorView;
+    }
+
+    @GetMapping(value = "/Dashboard")
+    public ModelAndView imsDashboard(){
+        ModelAndView imsView = new ModelAndView();
+        imsView.setViewName("dashboard.html");
+        return imsView;
     }
 }
