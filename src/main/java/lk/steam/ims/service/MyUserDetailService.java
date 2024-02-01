51505e446ee8dab0b50c9265d889e2ab36147e9a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 14:53:43 +0530

    renamed

--- src/main/java/lk/steam/ims/service/MyUserDetailService.java
+++ src/main/java/lk/steam/ims/service/MyUserDetailService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailService implements UserDetailsService {
+public class MyUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
