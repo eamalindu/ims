23ded6255e95b5e1e0bc04b9e25add27e8cfab8f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-02-01 14:18:17 +0530

    configuration added for the project [Important]

--- src/main/java/lk/steam/ims/config/WebConfig.java
+++ src/main/java/lk/steam/ims/config/WebConfig.java
@@ -1,9 +1,57 @@
 package lk.steam.ims.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(auth -> {
+            auth
+                    .requestMatchers("/login").permitAll()
+                    .requestMatchers("/Employee/**").hasAnyRole("Admin","Manager")
+                    .requestMatchers("/User/**").hasAnyRole("Admin","Manager")
+                    .requestMatchers("/error").permitAll()
+                    .requestMatchers("/resources/**").permitAll()
+                    .requestMatchers("/Dashboard").hasAnyRole("Admin","Manager","Counsellor")
+                    .anyRequest().authenticated();
+
+        })
+                //login handling
+                .formLogin(login ->{
+                    login
+                            .loginPage("/login")
+                            .defaultSuccessUrl("/Dashboard",true)
+                            .failureUrl("/login?error=failed")
+                            .usernameParameter("email")
+                            .passwordParameter("password");
+
+                })
+
+                //logout handling
+                .logout(logout->{
+                    logout
+                            .logoutSuccessUrl("/login")
+                            .logoutUrl("/logout");
+                })
+
+                //exception handling
+                .exceptionHandling(exception ->{
+                    exception.accessDeniedPage("/error");
+                })
+
+                //cross side reference
+                .csrf(csrf->{
+                    csrf.disable();
+                });
+
+        return httpSecurity.build();
+    }
 }
