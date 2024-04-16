f658c87a006b9606da8b2c721e4cb9ec8c69a42a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-16 12:50:36 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/schedules.js
+++ src/main/resources/static/resources/js/controllerJS/schedules.js
@@ -7,7 +7,32 @@ window.addEventListener("load",()=>{
     //get all the active batches (batches that can be used to register student) from the database
     const activeBatches = ajaxGetRequest("/Schedules/activeBatches")
 
+    displayPropertyListForBatches = [{property: getCourseName, dataType: 'function'}, {
+        property: 'batchCode', dataType: 'text'
+    }, {property: 'commenceDate', dataType: 'text'}, {property: 'endDate', dataType: 'text'}, {
+        property: getWeekDay, dataType: 'function'
+    }, {property: 'seatCount', dataType: 'text'}, {property: 'description', dataType: 'text'},];
+
+    fillDataIntoTableWithOutAction(tblBatches, activeBatches, displayPropertyListForBatches);
+
+    $('#tblBatches').DataTable();
+
 });
+//since we cant access the Course Name from the batches directly. creating a function to return the Course Name from the batches object
+const getCourseName = (ob) => {
+    return ob.courseID.name;
+}
+
+//since the isWeekday data type is in boolean we cant show true or false in the table
+//creating a function to return Weekday and Not Weekday based on their value
+const getWeekDay = (ob) => {
+    if (ob.isWeekday) {
+        return "Weekday";
+    } else {
+        return "Weekend";
+    }
+
+}
 
 
 const logout = ()=>{
