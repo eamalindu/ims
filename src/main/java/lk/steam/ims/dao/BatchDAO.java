db8e98369e103172d5896faa501157c72a558379
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-16 13:19:10 +0530

    new custom query added

--- src/main/java/lk/steam/ims/dao/BatchDAO.java
+++ src/main/java/lk/steam/ims/dao/BatchDAO.java
@@ -10,4 +10,7 @@ public interface BatchDAO extends JpaRepository<Batch,Integer> {
 
     @Query(value = "Select b from Batch b where (b.batchStatusID.id=1 or b.batchStatusID.id=2) and b.lastRegDate >= curdate()")
     List<Batch> getActiveBatches();
+
+    @Query(value = "SELECT b FROM Batch b WHERE b.courseID.id =?1 and (b.batchStatusID.id=1 or b.batchStatusID.id=2) and b.lastRegDate >= curdate()")
+    List<Batch> getActiveBatchesFromCourseID(Integer courseID);
 }
