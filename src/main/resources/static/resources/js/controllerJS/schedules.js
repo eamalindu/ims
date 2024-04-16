e4d09a2a3fb9d5bb0e562e19e9924102f32a6e05
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-04-16 12:36:17 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/schedules.js
+++ src/main/resources/static/resources/js/controllerJS/schedules.js
@@ -1,21 +1,14 @@
-$(document).ready(function () {
-    $(".chosen-inquiry").chosen({width: '100%'});
+window.addEventListener("load",()=>{
+
+    const courses = ajaxGetRequest("/course/findall");
+    fillSelectOptions(schedulesCourse,' ',courses,'name');
     $("#schedulesCourse").chosen({width: '25%'});
-    $('#inquiryTime').daterangepicker({
-        "minDate": new Date(),
-        "singleDatePicker": true,
-        "timePicker": true,
-        "timePicker24Hour": true,
-        "autoApply": true,
-        "linkedCalendars": false,
-        "showCustomRangeLabel": false,
-        "drops": "up",
-        "locale": {
-            "format": "YYYY-MM-DD [at] HH:mm"
-        }
-    });
+
+    //get all the active batches (batches that can be used to register student) from the database
+    const activeBatches = ajaxGetRequest("/Schedules/")
 
 });
+
 
 const logout = ()=>{
     showCustomConfirm("<strong class='text-lowercase'>"+btnProfileName.innerText+"</strong><br/>You are About to Logout from the System<br><br> are you sure?",function (result){
