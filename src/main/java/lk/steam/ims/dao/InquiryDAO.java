bd52c03f240984957d56f79049f9754371bfb5db
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-05-27 23:00:33 +0530

    edited

--- src/main/java/lk/steam/ims/dao/InquiryDAO.java
+++ src/main/java/lk/steam/ims/dao/InquiryDAO.java
@@ -44,6 +44,7 @@ public interface InquiryDAO extends JpaRepository<Inquiry,Integer> {
 
     //Display all the inquires with max follow-up data
     //This data will be shown in crm-> Dashboard-> schedule pool
+    //this should be edited logic is not correct
     @Query(value = "SELECT i.* FROM steam.inquiry i JOIN ( SELECT * FROM steam.followup WHERE DATE(nextfollowup) = CURDATE() ORDER BY id DESC LIMIT 1) AS followup ON i.id = followup.inquiry_id;",nativeQuery = true)
     List<Inquiry> test2();
 
