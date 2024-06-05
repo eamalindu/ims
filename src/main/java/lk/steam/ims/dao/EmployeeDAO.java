d8ba0ae4c91b51dd3cad0b98f10adac1d05bf5d6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-05 13:25:49 +0530

    new custom query

--- src/main/java/lk/steam/ims/dao/EmployeeDAO.java
+++ src/main/java/lk/steam/ims/dao/EmployeeDAO.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface EmployeeDAO extends JpaRepository<Employee,Integer> {
     @Query(value = "select * from employee where id not in (select employee_id from user where employee_id is not null)",nativeQuery = true)
     List<Employee> getEmployeesWithoutUserAccount();
+
+    @Query(value = "SELECT e from Employee e where e.designationID.id=2")
+    List<Employee> getActiveCounsellors();
 }
