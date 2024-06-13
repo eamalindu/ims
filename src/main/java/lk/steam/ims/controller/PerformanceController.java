4c526360f9157889eab60f493325d34d74becd68
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-13 22:24:25 +0530

    mapping added

--- src/main/java/lk/steam/ims/controller/PerformanceController.java
+++ src/main/java/lk/steam/ims/controller/PerformanceController.java
@@ -4,6 +4,7 @@ import lk.steam.ims.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +16,7 @@ public class PerformanceController {
     @Autowired
     private UserDAO userDAO;
 
+    @GetMapping
     public ModelAndView performanceUI(){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         ModelAndView imsPerformanceView = new ModelAndView();
