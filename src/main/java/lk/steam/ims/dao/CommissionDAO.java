e34eb19e8e2d08bfc8b94f12a41a537570e702af
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 14:06:29 +0530

    new custom query added

--- src/main/java/lk/steam/ims/dao/CommissionDAO.java
+++ src/main/java/lk/steam/ims/dao/CommissionDAO.java
@@ -4,8 +4,13 @@ import lk.steam.ims.entity.Commission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface CommissionDAO extends JpaRepository<Commission, Integer> {
 
     @Query("select c from Commission c where c.registrationID.id=?1")
     Commission getCommissionByRegistrationID(Integer registrationID);
+
+    @Query("select c from Commission c where date(c.timestamp)>=?1 and date(c.timestamp)<=?2 and c.paidTo=?3")
+    List<Commission> getCommissionByDateRangeAndPaidTo(String startDate, String endDate, String paidTo);
 }
