c1a22d78b01fb74cd0e464da809d02239f6aa6c7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 14:11:08 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/performance.js
+++ src/main/resources/static/resources/js/controllerJS/performance.js
@@ -1,3 +1,5 @@
 window.addEventListener('load',()=>{
-
+    const startDate = moment().startOf('month').format('YYYY-MM-DD');
+    const endDate = moment().endOf('month').format('YYYY-MM-DD');
+    const commissions = ajaxGetRequest("/Commission/getCommissionByDateRangeAndPaidTo/"+startDate+"/"+endDate);
 })
