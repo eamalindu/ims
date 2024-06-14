dc8325508261befc87ebedb8238aeb2546b40a9e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 14:30:04 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/performance.js
+++ src/main/resources/static/resources/js/controllerJS/performance.js
@@ -1,5 +1,10 @@
 window.addEventListener('load',()=>{
     const startDate = moment().startOf('month').format('YYYY-MM-DD');
     const endDate = moment().endOf('month').format('YYYY-MM-DD');
+    let commissionTotal = 0;
     const commissions = ajaxGetRequest("/Commission/getCommissionByDateRangeAndPaidTo/"+startDate+"/"+endDate);
+    commissions.forEach(commission => {
+        commissionTotal += commission.amount;
+    });
+    commissionTotalText.innerText = "Rs. "+commissionTotal.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 })
