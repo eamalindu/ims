7c13abb99d3178693ae365b17606efc328f03dba
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 19:00:17 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/performance.js
+++ src/main/resources/static/resources/js/controllerJS/performance.js
@@ -2,6 +2,7 @@ window.addEventListener('load',()=>{
     const startDate = moment().startOf('month').format('YYYY-MM-DD');
     const endDate = moment().endOf('month').format('YYYY-MM-DD');
     generatePerformanceChart(startDate,endDate);
+    resetSearchBar();
     let commissionTotal = 0;
     const commissions = ajaxGetRequest("/Commission/getCommissionByDateRangeAndPaidTo/"+startDate+"/"+endDate);
     commissions.forEach(commission => {
@@ -44,6 +45,29 @@ const generatePerformanceChart = (startDate,endDate)=>{
     conversionRate.innerText = ((inquiryCount[4]/totalInquiryCount)*100).toFixed(2)+'%';
 
 
+}
 
+const resetSearchBar = ()=>{
+    var start = moment();
+    var end = moment();
 
+    function cb(start, end) {
+        $('#registrationSearchDateRange span').html(start.format('YYYY-MMMM-DD') + ' - ' + end.format('YYYY-MMMM-DD'));
+    }
+
+    $('#registrationSearchDateRange').daterangepicker({
+        startDate: start, endDate: end, locale: {
+            "format": "YYYY-MM-DD",
+        }, ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+            'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
+        }
+    }, cb);
+
+    cb(start, end);
 }
