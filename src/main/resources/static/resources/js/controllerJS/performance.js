d1ee7168e9b75e4907ba198e078dc184458da58f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 16:56:23 +0530

    color changed to 553772

--- src/main/resources/static/resources/js/controllerJS/performance.js
+++ src/main/resources/static/resources/js/controllerJS/performance.js
@@ -23,6 +23,6 @@ const generatePerformanceChart = (startDate,endDate)=>{
     console.log(inquiryStatusNames);
     console.log(inquiryCount);
 
-    generateChart(chartPerformance,'',inquiryStatusNames,'Inquiry Count',[{name:'',data:inquiryCount,color:'#007bff'}]);
+    generateChart(chartPerformance,'',inquiryStatusNames,'Inquiry Count',[{name:'',data:inquiryCount,color:'#553772'}]);
 
 }
