2045c8aa1a4a59aa650aafa15e6c7c057e1e8a67
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 16:49:28 +0530

    major update

--- src/main/resources/static/resources/js/controllerJS/performance.js
+++ src/main/resources/static/resources/js/controllerJS/performance.js
@@ -1,6 +1,7 @@
 window.addEventListener('load',()=>{
     const startDate = moment().startOf('month').format('YYYY-MM-DD');
     const endDate = moment().endOf('month').format('YYYY-MM-DD');
+    generatePerformanceChart(startDate,endDate);
     let commissionTotal = 0;
     const commissions = ajaxGetRequest("/Commission/getCommissionByDateRangeAndPaidTo/"+startDate+"/"+endDate);
     commissions.forEach(commission => {
@@ -8,3 +9,18 @@ window.addEventListener('load',()=>{
     });
     commissionTotalText.innerText = "Rs. "+commissionTotal.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 })
+
+
+const generatePerformanceChart = (startDate,endDate)=>{
+
+    const inquiryStatus = ajaxGetRequest("/InquiryStatus/findall");
+    let inquiryStatusNames = [];
+    let inquiryCount = [];
+    inquiryStatus.forEach(inquiry => {
+        inquiryStatusNames.push(inquiry.name);
+        inquiryCount.push(ajaxGetRequest("/Inquiry/getInquiryByDateRangeAndStatus/"+startDate+"/"+endDate+"/"+inquiry.name).length);
+    })
+    console.log(inquiryStatusNames);
+    console.log(inquiryCount);
+
+}
