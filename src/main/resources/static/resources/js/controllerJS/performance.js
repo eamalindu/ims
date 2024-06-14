1719ea813162d3c2f628d80f329f457a393e2bd2
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 16:53:46 +0530

    major update

--- src/main/resources/static/resources/js/controllerJS/performance.js
+++ src/main/resources/static/resources/js/controllerJS/performance.js
@@ -23,4 +23,6 @@ const generatePerformanceChart = (startDate,endDate)=>{
     console.log(inquiryStatusNames);
     console.log(inquiryCount);
 
+    generateChart(chartPerformance,'',inquiryStatusNames,'Inquiry Count',[{name:'',data:inquiryCount,color:'#007bff'}]);
+
 }
