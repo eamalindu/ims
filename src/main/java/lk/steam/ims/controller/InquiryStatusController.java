e55826b7df3f41e41cad5015aa9afed3055acc2a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-14 15:55:25 +0530

    controller added

--- src/main/java/lk/steam/ims/controller/InquiryStatusController.java
+++ src/main/java/lk/steam/ims/controller/InquiryStatusController.java
@@ -1,4 +1,23 @@
 package lk.steam.ims.controller;
 
+import lk.steam.ims.dao.InquiryStatusDAO;
+import lk.steam.ims.entity.InquiryStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/InquiryStatus")
 public class InquiryStatusController {
+
+    @Autowired
+    private InquiryStatusDAO inquiryStatusDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<InquiryStatus> findAll(){
+        return inquiryStatusDAO.findAll();
+    }
 }
