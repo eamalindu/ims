5f15bce0c853e402da76fd826ea347703f3420fc
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-16 13:35:15 +0530

    custom query added

--- src/main/java/lk/steam/ims/dao/RoleDAO.java
+++ src/main/java/lk/steam/ims/dao/RoleDAO.java
@@ -3,7 +3,10 @@ package lk.steam.ims.dao;
 
 import lk.steam.ims.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RoleDAO extends JpaRepository<Role,Integer> {
 
+    @Query("SELECT r from Role r where r.designation=?1")
+    Role getRoleByName(String designation);
 }
