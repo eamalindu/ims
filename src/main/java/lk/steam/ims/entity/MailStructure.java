d1a6a727fc7a1d793eb9d8d06f7d21fa65e66d51
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-06-17 20:44:49 +0530

    comments added

--- src/main/java/lk/steam/ims/entity/MailStructure.java
+++ src/main/java/lk/steam/ims/entity/MailStructure.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//Using @AllArgsConstructor, @NoArgsConstructor, @Data annotations from Lombok to avoid boilerplate code
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+//Creating a class called MailStructure which contains two attributes
 public class MailStructure {
+    //subject - String type to store the subject of the email
     private String subject;
+    //message - String to store the message of the email
     private String message;
 }
