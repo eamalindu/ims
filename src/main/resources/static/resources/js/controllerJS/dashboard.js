a3ed2685fc69ac7528900e1bd99a23b6787039ea
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-19 12:07:22 +0530

    dataTable added

--- src/main/resources/static/resources/js/controllerJS/dashboard.js
+++ src/main/resources/static/resources/js/controllerJS/dashboard.js
@@ -30,6 +30,11 @@ const refreshInquiryPoolTable = () => {
 
     fillDataIntoTable(tblInquiryPool, newInquiries, displayPropertyListForInquiryPool, rowView,'offCanvasInquirySheet');
 
+    //initiate dataTable only if inquires are available
+    if(newInquiries.length>0){
+        $('#tblInquiryPool').dataTable();
+    }
+
     //testing code start
 
     scheduledInquiries = ajaxGetRequest("/Inquiry/getInquiriesWithFollowUpsToday")
@@ -38,6 +43,11 @@ const refreshInquiryPoolTable = () => {
         property: getCourse, dataType: 'function'
     }, {property: getSource, dataType: 'function'}, {property: getFullName, dataType: 'function'}];
     fillDataIntoTable(tblScheduledPool, scheduledInquiries, displayPropertyListForScheduledPool, rowView,'offCanvasInquirySheet');
+
+    //initiate dataTable only if inquires are available
+    if(scheduledInquiries.length>0){
+        $('#tblScheduledPool').dataTable();
+    }
 
 //testing code end
 
