51231388e3e9368c4f75155c371a74dc564f377d
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-21 20:13:29 +0530

    edited

--- src/main/java/lk/steam/ims/dao/ModuleDAO.java
+++ src/main/java/lk/steam/ims/dao/ModuleDAO.java
@@ -2,6 +2,12 @@ package lk.steam.ims.dao;
 
 import lk.steam.ims.entity.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ModuleDAO extends JpaRepository<Module,Integer> {
+
+    @Query("select m from Module m where m.id not in (select p.moduleID.id from Privilege p where p.roleID.id=?1)")
+    List<Module> getModuleByRoles(Integer roleID);
 }
