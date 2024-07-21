a5613550a8af94e165caac4ece09b24c17b88072
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-21 20:06:59 +0530

    edited

--- src/main/java/lk/steam/ims/dao/RoleDAO.java
+++ src/main/java/lk/steam/ims/dao/RoleDAO.java
@@ -5,8 +5,13 @@ import lk.steam.ims.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface RoleDAO extends JpaRepository<Role,Integer> {
 
     @Query("SELECT r from Role r where r.name=?1")
     Role getRoleByName(String designation);
+
+    @Query("SELECT r from Role r where r.name <> 'Admin' ")
+    List<Role> getRolesWithoutAdmin();
 }
