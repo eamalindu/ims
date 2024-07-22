4dff10dde32921ac7f8a84aab5dbe04d33a5417a
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-22 22:21:59 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/reportCommission.js
+++ src/main/resources/static/resources/js/controllerJS/reportCommission.js
@@ -2,7 +2,7 @@ window.addEventListener('load',()=> {
     resetReportSearchBar();
     const startDate = moment().startOf('month').format('YYYY-MM-DD');
     const endDate = moment().endOf('month').format('YYYY-MM-DD');
-
+    generateCommissionReport(startDate,endDate);
 
 });
 
@@ -30,4 +30,13 @@ const resetReportSearchBar = ()=>{
     }, cb);
 
     cb(start, end);
+}
+
+const generateCommissionReport = (startDate,endDate)=>{
+    let commissionTotal = 0;
+    const commissions = ajaxGetRequest("/Commission/getCommissionByDateRangeAndPaidTo/"+startDate+"/"+endDate);
+    commissions.forEach(commission => {
+        commissionTotal += commission.amount;
+    });
+    commissionTotalText.innerText = "Rs. "+commissionTotal.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 }
