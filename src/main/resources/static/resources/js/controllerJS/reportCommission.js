416d7415ece6815179fac178a572431a447222d6
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-22 22:11:32 +0530

    edited

---
+++ src/main/resources/static/resources/js/controllerJS/reportCommission.js
@@ -0,0 +1,33 @@
+window.addEventListener('load',()=> {
+    resetReportSearchBar();
+    const startDate = moment().startOf('month').format('YYYY-MM-DD');
+    const endDate = moment().endOf('month').format('YYYY-MM-DD');
+
+
+});
+
+
+const resetReportSearchBar = ()=>{
+    var start = moment().startOf('month');
+    var end = moment().endOf('month');
+
+    function cb(start, end) {
+        $('#commissionSearchDateRange span').html(start.format('YYYY-MMMM-DD') + ' - ' + end.format('YYYY-MMMM-DD'));
+    }
+
+    $('#commissionSearchDateRange').daterangepicker({
+        startDate: start, endDate: end, locale: {
+            "format": "YYYY-MM-DD",
+        }, ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+            'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
+        }
+    }, cb);
+
+    cb(start, end);
+}
