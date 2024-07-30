01ef6e77008fc1184aad1cbedea5889e18954962
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-30 09:56:18 +0530

    edited

--- src/main/resources/static/resources/js/controllerJS/commissionRate.js
+++ src/main/resources/static/resources/js/controllerJS/commissionRate.js
@@ -9,9 +9,9 @@ const  refreshCommissionRateTable = ()=>{
     const commissionRates = ajaxGetRequest("/CommissionRate/findAll");
     const displayPropertyListForCommissionRate = [
         {property:getCourseName,dataType:'function'},
-        {property:'fullPaymentRate',dataType:'text'},
-        {property:'partPaymentRate',dataType:'text'},
-        {property:'timestamp',dataType:'text'},
+        {property:getFullPaymentRate,dataType:'function'},
+        {property:getPartPaymentRate,dataType:'function'},
+        {property:getTimeStamp,dataType:'function'},
         {property:'addedBy',dataType:'text'},
     ];
     fillDataIntoTable(tblCommissionRate,commissionRates,displayPropertyListForCommissionRate,rowView,'offCanvasCommissionRate');
@@ -27,7 +27,19 @@ const resetCommissionRateForm = ()=>{
 }
 
 const getCourseName = (ob)=>{
-return ob.courseID.name;
+    return ob.courseID.name;
+}
+
+const getTimeStamp = (ob)=>{
+    return ob.timestamp.replace("T"," ");
+}
+
+const getFullPaymentRate = (ob)=>{
+    return "Rs. "+ob.fullPaymentRate.toLocaleString('en-US',{minimumFractionDigits: 2});
+}
+
+const getPartPaymentRate = (ob)=>{
+    return "Rs. "+ob.partPaymentRate.toLocaleString('en-US',{minimumFractionDigits: 2});
 }
 
 const rowView = (ob)=>{
