df735e04e6a10de3ee650353ec2a302fa43521d7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-07-30 15:39:16 +0530

    edited

--- src/main/java/lk/steam/ims/controller/CommissionController.java
+++ src/main/java/lk/steam/ims/controller/CommissionController.java
@@ -1,6 +1,7 @@
 package lk.steam.ims.controller;
 
 import lk.steam.ims.dao.CommissionDAO;
+import lk.steam.ims.dao.UserDAO;
 import lk.steam.ims.entity.Commission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Base64;
 import java.util.List;
 
 @RestController
@@ -18,6 +21,8 @@ public class CommissionController {
 
     @Autowired
     private CommissionDAO commissionDAO;
+    @Autowired
+    private UserDAO userDAO;
 
     @GetMapping("/getCommissionByRegistrationID/{registrationID}")
     public Commission getCommissionByRegistrationID(@PathVariable Integer registrationID) {
@@ -28,6 +33,27 @@ public class CommissionController {
     public List<Commission> getCommissionByDateRangeAndPaidTo(@PathVariable String startDate, @PathVariable String endDate) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return commissionDAO.getCommissionByDateRangeAndPaidTo(startDate, endDate, authentication.getName());
+    }
+
+    @GetMapping()
+    public ModelAndView commissionUI(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        ModelAndView commissionView = new ModelAndView();
+        commissionView.setViewName("commission.html");
+
+        commissionView.addObject("username",auth.getName());
+        commissionView.addObject("title","Commissions | STEAM IMS");
+        commissionView.addObject("activeNavItem","commissionAll");
+        String loggedInEmployeeName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getFullName();
+        String loggedInDesignationName = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getDesignationID().getDesignation();
+        byte[] photoBytes = userDAO.getUserByUsername(auth.getName()).getEmployeeID().getPhotoPath();
+        String base64Image = Base64.getEncoder().encodeToString(photoBytes);
+        String imageSrc = "data:image/png;base64," + base64Image;
+        commissionView.addObject("loggedInEmployeeName",loggedInEmployeeName);
+        commissionView.addObject("loggedInDesignationName",loggedInDesignationName);
+        commissionView.addObject("loggedInImage",imageSrc);;
+        return commissionView;
     }
 
 }
