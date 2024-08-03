a9204e65d4dc9151aac4a4fe5eb8c3283616d503
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-08-03 20:02:48 +0530

    edited

--- src/main/java/lk/steam/ims/controller/InquiryController.java
+++ src/main/java/lk/steam/ims/controller/InquiryController.java
@@ -61,6 +61,7 @@ public class InquiryController {
         return inquiryDAO.findProcessingInquiry();
     }
 
+    //get inquiries by date range and status
     @GetMapping(value = "/getInquiryByDateRangeAndStatus/{startDate}/{endDate}/{status}",produces = "application/json")
     public List<Inquiry> getInquiryByDateRangeAndStatus(@PathVariable String startDate, @PathVariable String endDate, @PathVariable String status){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -69,6 +70,7 @@ public class InquiryController {
         return inquiryDAO.getInquiryByDateRangeAndStatus(start,end,status,auth.getName());
     }
 
+    //get inquiries by date range and status and addedBy
     @GetMapping(value = "/getInquiryByDateRangeAndStatusAndAddedBy/{startDate}/{endDate}/{status}/{addedBy}",produces = "application/json")
     public List<Inquiry> getInquiryByDateRangeAndStatusAndAddedBy(@PathVariable String startDate, @PathVariable String endDate, @PathVariable String status,@PathVariable String addedBy){
         LocalDate  start = LocalDate.parse(startDate);
@@ -81,58 +83,69 @@ public class InquiryController {
         return inquiryDAO.test();
     }
 
+    //get inquiries that have followups today
     @GetMapping(value = "/getInquiriesWithFollowUpsToday",produces = "application/json")
     public List<Inquiry> getInquiriesWithFollowUpsToday(){
         return inquiryDAO.getInquiriesWithFollowUpsToday();
     }
 
+    //get new inquiries count
     @GetMapping(value = "/newinquirycount",produces = "application/json")
     public String newInquiryCount(){
         return inquiryDAO.getNewInquiryCount();
     }
 
+    //get counsellors by date range
     @GetMapping(value = "/getCounsellors/{startDate}/{endDate}",produces = "application/json")
     public List<String> getCounsellorsByDates(@PathVariable String startDate,@PathVariable String endDate){
         return inquiryDAO.getCounsellorsByDates(startDate,endDate);
     }
 
+    //get inquiries by date range and counsellor
     @GetMapping(value = "/getInquiriesByDateRangeAndCourse/{startDate}/{endDate}/{courseId}",produces = "application/json")
     public List<Inquiry> getInquiriesByDateRangeAndCourse(@PathVariable String startDate,@PathVariable String endDate,@PathVariable Integer courseId){
         return inquiryDAO.getInquiriesByDateRangeAndCourse(startDate,endDate,courseId);
     }
 
+    //get inquiries by date range and course for logged in user
     @GetMapping(value = "/getInquiriesByDateRangeAndCourseForLoggedInUser/{startDate}/{endDate}/{courseId}",produces = "application/json")
     public List<Inquiry> getInquiriesByDateRangeAndCourseForLoggedInUser(@PathVariable String startDate,@PathVariable String endDate,@PathVariable Integer courseId){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return inquiryDAO.getInquiriesByDateRangeAndCourseForLoggedInUser(startDate,endDate,courseId,auth.getName());
     }
 
+    //get inquiries by date range and source
     @GetMapping(value = "/getInquiriesByDateRangeAndSource/{startDate}/{endDate}/{sourceID}",produces = "application/json")
     public List<Inquiry> getInquiriesByDateRangeAndSource(@PathVariable String startDate,@PathVariable String endDate,@PathVariable Integer sourceID){
         return inquiryDAO.getInquiriesByDateRangeAndSource(startDate,endDate,sourceID);
     }
 
+    //get registered inquiries this month
     @GetMapping(value="/registeredInquiriesThisMonth/{startDate}/{endDate}",produces = "application/json")
     public List<Inquiry> getRegisteredInquiriesThisMonth(@PathVariable String startDate,@PathVariable String endDate){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return inquiryDAO.getRegisteredInquiriesThisMonth(startDate,endDate,auth.getName());
     }
 
+    //get all inquireis by date range
     @GetMapping(value = "/getAllInquiriesByDateRange/{startDate}/{endDate}",produces = "application/json")
     public List<Inquiry> getAllInquiriesByDateRange(@PathVariable String startDate,@PathVariable String endDate){
         return inquiryDAO.getAllInquiriesByDateRange(startDate,endDate);
     }
 
+    //get all inquiries by date range and status
     @GetMapping(value = "/getAllInquiriesByDateRangeAndStatus/{startDate}/{endDate}/{status}",produces = "application/json")
     public List<Inquiry> getAllInquiriesByDateRangeAndStatus(@PathVariable String startDate,@PathVariable String endDate, @PathVariable String status){
         return inquiryDAO.getAllInquiriesByDateRangeAndStatus(startDate,endDate,status);
     }
 
+    //get all inquiries without followUps by date range
     @GetMapping(value = "/getDroppedInquiriesWithoutFollowUpsByDateRange/{startDate}/{endDate}",produces = "application/json")
         public List<Inquiry> getDroppedInquiriesWithoutFollowUpsByDateRange(@PathVariable String startDate,@PathVariable String endDate){
         return inquiryDAO.getDroppedInquiriesWithoutFollowUpsByDateRange(startDate,endDate);
     }
 
+    //get inquiry from inquiry number
     @GetMapping(value="/getInquiryNumberByID/{id}",produces = "application/json")
     public Inquiry getInquiryNumberById(@PathVariable Integer id){
         return inquiryDAO.getInquiryNumberById(id);
@@ -144,21 +157,28 @@ public class InquiryController {
         return inquiryDAO.searchInquiry(startDate,endDate,sourceID,courseID,addedBy,input);
     }
 
+    //search an inquiry by the given input
+    //ok for nic, inquiry number and mobile number
     @GetMapping(value = "/searchInquiryByInput/{input}",produces = "application/json")
     public List<Inquiry> searchInquiryByInput(@PathVariable String input){
         return inquiryDAO.searchInquiryByInput(input);
     }
+    //search an inquiry by the given input and date range
+    //ok for nic, inquiry number and mobile number
     @GetMapping(value = "/searchInquiryByDateRangeAndInput/{startDate}/{endDate}/{input}",produces = "application/json")
     public List<Inquiry> searchInquiryByDateRangeAndInput(@PathVariable String startDate,@PathVariable String endDate,@PathVariable String input){
         return inquiryDAO.searchInquiryByDateRangeAndInput(startDate,endDate,input);
     }
 
-    
+    //save new inquiry
     @PostMapping
     public String saveNewInquiry(@RequestBody Inquiry inquiry){
+        //get the logged user
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        //get the logged user privilege
         Privilege loggedUserPrivilege = privilegeController.getPrivilegeByUserAndModule(auth.getName(),"INQUIRY");
 
+        //check if the logged user has the insert privilege
         if(!loggedUserPrivilege.getInsertPrivilege()){
             return "<br>User does not have sufficient privilege.";
         }
@@ -188,21 +208,26 @@ public class InquiryController {
                 inquiry.setInquiryNumber(inqNextNumber);
             }
 
-
+            //save inquiry
             inquiryDAO.save(inquiry);
             return "OK";
         }
+        //error handling
         catch (Exception ex){
             return "Save Failed "+ex.getMessage();
         }
     }
 
 
+    //update inquiry
     @PutMapping
     public String updateInquiry(@RequestBody Inquiry inquiry){
+        //get the logged user
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        //get the logged user privilege
         Privilege loggedUserPrivilege = privilegeController.getPrivilegeByUserAndModule(auth.getName(),"INQUIRY");
 
+        //check if the logged user has the update privilege
         if(!loggedUserPrivilege.getUpdatePrivilege()){
             return "<br>User does not have sufficient privilege.";
         }
@@ -212,6 +237,7 @@ public class InquiryController {
             if(existInquiry!=null && existInquiry.getId()!=inquiry.getId()){
                 return "<br>Updated Failed!<br>Inquiry With Same Course and NIC already exist in the system";
             }
+            //save inquiry
             inquiryDAO.save(inquiry);
             return "OK";
 
@@ -221,6 +247,7 @@ public class InquiryController {
         }
 
     }
+    //delete inquiry
     @DeleteMapping
     public String deleteInquiry(@RequestBody Inquiry inquiry){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -235,6 +262,7 @@ public class InquiryController {
         }
         try {
             //no need to check anything, because there are no any unique values
+            //soft delete
             inquiry.setInquiryStatusId(inquiryStatusDAO.getReferenceById(4));
             inquiryDAO.save(inquiry);
             return "OK";
